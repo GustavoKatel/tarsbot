1f549b26a724087482b746a2308cfad69b9f3f3b
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-04-30 20:13:11 -0300

    Pomodoro: Add interval checker

--- cmdlets/pomodoro.js
+++ cmdlets/pomodoro.js
@@ -5,6 +5,14 @@ const cbAnswerMiddleware = (ctx, next) => {
     next();
 };
 
+const dd = (date) => {
+    if(date) {
+        return date.toLocaleString();
+    }
+
+    return '-';
+}
+
 const pomodoro = {
     name: 'pomodoro',
 
@@ -13,6 +21,31 @@ const pomodoro = {
         lunchStart: null,
         lunchEnd: null,
         end: null
+    },
+
+    intervalCheckerHandle: null,
+
+    intervalChecker: (ctx) => {
+        let start = pomodoro.pomodoroConfig.lunchStart;
+        if(!start) {
+            if (pomodoro.intervalCheckerHandle) {
+                clearInterval(pomodoro.intervalCheckerHandle);
+                pomodoro.intervalCheckerHandle = null;
+            }
+            return;
+        }
+
+        let end = pomodoro.pomodoroConfig.lunchEnd || new Date();
+        
+        // Check if we reached 30min
+        let diff = ((end - start) / 1000) / 60;
+        if(diff >= 30) {
+            ctx.replyWithMarkdown('🍴 *30min lunch interval reached*');
+            if (pomodoro.intervalCheckerHandle) {
+                clearInterval(pomodoro.intervalCheckerHandle);
+                pomodoro.intervalCheckerHandle = null;
+            }
+        }
     },
     
     init: (app) => {
@@ -34,6 +67,13 @@ const pomodoro = {
                 pomodoro.pomodoroConfig.lunchStart = null;
                 pomodoro.pomodoroConfig.lunchEnd = null;
                 pomodoro.pomodoroConfig.end = null;
+
+                pomodoro.intervalCheckerHandle = setInterval(
+                    pomodoro.intervalChecker,
+                    1000*60,
+                    ctx
+                );
+                
             }
         );
         
@@ -61,47 +101,50 @@ const pomodoro = {
             }
         );
 
+        let queryMiddleware = (ctx) => {
+            let msg = '🍎 *Pomodoro Status*\n\n';
+
+            msg += `*Start*: ${dd(pomodoro.pomodoroConfig.start)}\n\n`;
+            msg += `*Lunch Start*: ${dd(pomodoro.pomodoroConfig.lunchStart)}\n\n`;
+
+            let lunchInterval = 0;
+            if (pomodoro.pomodoroConfig.lunchStart) {
+                // Lunch has started and finished
+                // Diff in minutes
+                let end = pomodoro.pomodoroConfig.lunchEnd || new Date();
+                let diff = Math.abs(end - pomodoro.pomodoroConfig.lunchStart);
+                lunchInterval = Math.floor((diff / 1000) / 60);
+            }
+            msg += `_Interval_: ${lunchInterval}min\n\n`;
+
+            msg += `*Lunch End*: ${dd(pomodoro.pomodoroConfig.lunchEnd)}\n\n`;
+            msg += `*End*: ${dd(pomodoro.pomodoroConfig.end)}\n\n`;
+
+            let total = 0;
+
+            if (pomodoro.pomodoroConfig.start) {
+                let end = pomodoro.pomodoroConfig.end || new Date();
+                let diff = Math.abs(end - pomodoro.pomodoroConfig.start);
+                total = Math.floor((diff / 1000) / 60);
+                total -= lunchInterval;
+            }
+
+            let h = Math.floor(total / 60);
+            msg += `_Total_: ${h}h${total % 60}m\n`;
+
+            ctx.replyWithMarkdown(msg);
+        };
         app.action(
             '/pomodoro query',
             cbAnswerMiddleware,
-            (ctx) => {
-                let msg = '🍎 *Pomodoro Status*\n';
-
-                msg += `*Start*: ${pomodoro.pomodoroConfig.start || '-'}\n`;
-                msg += `*Lunch Start*: ${pomodoro.pomodoroConfig.lunchStart || '-'}\n`;
-                
-                let lunchInterval = 0;
-                if(pomodoro.pomodoroConfig.lunchStart) {
-                    // Lunch has started and finished
-                    // Diff in minutes
-                    let end = pomodoro.pomodoroConfig.lunchEnd || new Date();
-                    let diff = Math.abs(end - pomodoro.pomodoroConfig.lunchStart);
-                    lunchInterval = Math.floor((diff / 1000) / 60);
-                }
-                msg += `_Interval_: ${lunchInterval}min\n`;
-                
-                msg += `*Lunch End*: ${pomodoro.pomodoroConfig.lunchEnd || '-'}\n`;
-                msg += `*End*: ${pomodoro.pomodoroConfig.end || '-'}\n`;
-                
-                let total = 0;
-
-                if(pomodoro.pomodoroConfig.start) {
-                    let end = pomodoro.pomodoroConfig.end || new Date();
-                    let diff = Math.abs(end - pomodoro.pomodoroConfig.start);
-                    total = Math.floor((diff / 1000) / 60);
-                    total -= lunchInterval;
-                }
-                
-                let h = Math.floor(total/60);
-                msg += `_Total_: ${h}h${total%60}m\n`;
-                
-                ctx.replyWithMarkdown(msg);
-            }
+            queryMiddleware
         );
+        app.hears('🍎?', queryMiddleware);
     },
 
     buttons: [
         '🍎',
+        '🍎?'
     ],
 
     inlineButtons: [
